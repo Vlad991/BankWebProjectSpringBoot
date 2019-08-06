d308d436ec3c966a10d9de6566cd85a8696a41db
vlad <vladkuzma99@gmail.com>
2019-08-06 16:49:36 +0300

    Add websocket configuration

---
+++ src/main/java/com/mybank/configuration/WebSocketConfiguration.java
@@ -0,0 +1,30 @@
+package com.mybank.configuration;
+
+import com.mybank.controller.WebSocketController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    @Value("${mybank.serviceName}")
+    private String serviceName;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(getWebSocketController(), "/socket")
+                .setAllowedOrigins("*")
+                .addInterceptors(new SecurityInterceptor(serviceName))
+                .withSockJS();
+    }
+
+    @Bean
+    public WebSocketController getWebSocketController(){
+        return new WebSocketController();
+    }
+}
