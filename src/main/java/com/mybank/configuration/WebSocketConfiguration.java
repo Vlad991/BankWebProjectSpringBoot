a94a31f192499caeef8e7ca643a994516be7d209
vlad <vladkuzma99@gmail.com>
2019-08-09 22:42:48 +0300

    Add trial spring security

--- src/main/java/com/mybank/configuration/WebSocketConfiguration.java
+++ src/main/java/com/mybank/configuration/WebSocketConfiguration.java
@@ -1,6 +1,7 @@
 package com.mybank.configuration;
 
 import com.mybank.controller.WebSocketController;
+import com.mybank.interceptor.SecurityInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
