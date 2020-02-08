bd719563c7dda04769567a4e0df030210d61291e
Vlad991 <vladkuzma99@gmail.com>
2020-02-08 19:52:49 +0200

    Finish debug

--- src/main/java/com/mybank/Application.java
+++ src/main/java/com/mybank/Application.java
@@ -1,24 +1,12 @@
 package com.mybank;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
     }
 }
--- src/main/java/com/mybank/exception/UserNotFoundException.java
+++ src/main/java/com/mybank/exception/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.mybank.exception;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(){
+    public UserNotFoundException() {
         super();
     }
 }
